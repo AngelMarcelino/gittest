f414ae369b77df2e40b9e391245308f38d656769
Angel Marcelino Gonzalez Mayoral <agemarcelino@hotmail.com>
2018-05-31 13:41:08 -0500

    Initial create

---
+++ index.js
@@ -0,0 +1,7 @@
+import Footer from './app/components/footer-component';
+import Main from './app/components/main-component';
+import Sidebar from './app/components/sidebar-component';
+
+console.log(Footer);
+console.log(Main);
+console.log(Sidebar);
